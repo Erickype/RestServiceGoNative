f40bab6b61d9a28895408cb1c96cfa40e95630c2
Erickype <erickype@hotmail.com>
2022-08-15 12:03:57 -0500

    Server init config

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"net/http"
+)
 
+func main() {
+	server := http.Server{
+		Addr: ":8080",
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		panic(err)
+	}
 }
