259a02b8f0eef786d409b33e8e2e6df8ab410733
Erickype <erickype@hotmail.com>
2022-08-15 13:02:02 -0500

    Clean up and user of the NewServer function

--- main.go
+++ main.go
@@ -1,27 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"github.com/Erickype/RestServiceGoNative/server"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
-		if r.Method != "GET" {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			fmt.Fprintf(w, "Method not allowed")
-			return
-		}
-
-		fmt.Fprintf(w, "Hello there %s", "visitor")
-	})
-
 	// server init config
-	server := http.Server{
-		Addr: ":8080",
-	}
+	server := server.NewServer(":8080")
 
 	err := server.ListenAndServe()
 
