2428f7b2e4d6b2143e46d732f9e888503b74334d
Erickype <erickype@hotmail.com>
2022-08-15 13:07:44 -0500

    Countrie struct

--- server/server.go
+++ server/server.go
@@ -2,6 +2,13 @@ package server
 
 import "net/http"
 
+type Country struct {
+	Name     string
+	Languaje string
+}
+
+var countries []*Country = []*Country{}
+
 func NewServer(addr string) *http.Server {
 	InitRoutes()
 	return &http.Server{
