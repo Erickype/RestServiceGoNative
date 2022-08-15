f96eae592f4da20a0dcecb726c47c5e3e7ca043b
Erickype <erickype@hotmail.com>
2022-08-15 12:13:03 -0500

    Default handle function

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Method != "GET" {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			fmt.Fprintf(w, "Method not allowed")
+			return
+		}
+
+		fmt.Fprintf(w, "Hello there %s", "visitor")
+	})
+
+	// server init config
 	server := http.Server{
 		Addr: ":8080",
 	}
